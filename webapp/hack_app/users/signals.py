325e2250da985ec2c27d821318d66b8168ce3560
Orestis Pourgouridis <sc16op@leeds.ac.uk>
2019-03-13 15:29:29 +0000

    Background Reasearch

---
+++ webapp/hack_app/users/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+#when a user is saved send the post_save signal received by this function (receiver)
+#the fun arguments are passed by post_save signal
+#kwargs accepts any additional keyword arguments
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    #if user was created
+    if created:
+        #create a Profile project with the user = the instance of user created
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
